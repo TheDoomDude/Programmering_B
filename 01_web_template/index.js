a88a296469bfd31d48308ad39e2df5d4663dda10
TheDoomDude <tobiasld2008@gmail.com>
2025-10-31 11:30:58 +0100

    Video

--- 01_web_template/index.js
+++ 01_web_template/index.js
@@ -3,19 +3,28 @@ var currentPage ='#page5'
 //P5 setup() bliver kaldt en gang føre siden vises
 function setup(){
     console.log('P5 setup kaldt')
-    //set event listerers op på menu
-   var allMenuItems = selectAll('.sidebar a')
-    console.log(allMenuItems)
-    allMenuItems.map( 
-        function(item){
-            item.mousePressed(
-                function(){
-                    shiftPage(item.attribute('action'))
-                }
+    //skift til current page
+    shiftPage(currentPage)
+    //set menu op
+    //Hent alle sider som et array
+    var allPages = selectAll('.page')
+    //Løb listen igennem for en
+    allPages.map(
+        page =>  {
+            //Lav et nyt <a> element
+            var menuItem = createElement('a')
+            //Sæt a taggets html til sidens titel
+            menuItem.html(page.attribute('title'))
+            //Sæt evenlistener på a tagget
+            menuItem.mousePressed(
+                ()=> shiftPage('#' + page.attribute('id'))
             )
+            //Sæt a tagget ind i sidebaren
+            select('.sidebar').child(menuItem)
         }
     )
 }
+
 
 
 function shiftPage(newPage){
